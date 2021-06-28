eb187351f7784d7e2367fd92801210f6d7ebe724
bryce_boddy <bryce.boddy@gmail.com>
2021-06-27 17:13:39 -0700

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   helper_method :cart
   def cart
-    session[:cart] ||= []
+    session[:cart] ? session[:cart] : session[:cart] = []
   end
 end
